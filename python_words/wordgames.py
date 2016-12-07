38172c95a19a83768a9c907efa973b037273f67c
Pugilem <gytenis98@gmail.com>
2016-12-06 22:02:26 -0600

    Revert "_ for letters implemented"
    This reverts commit 121908cf82cfd1a10b7c0df014c014cf38218644.

--- python_words/wordgames.py
+++ python_words/wordgames.py
@@ -1,54 +1,11 @@
-
-import random
-
 def get_random_word():
-    words = ["pizza", "cheese", "apples", "nuts", "tomatoes", "turkey"]
-    word = words[random.randint(0,len(words)-1)]
-    return word
-
-def show_word(word):
-    for character in word:
-        print(character, " ", end="")
-    print("")
-
-def get_guess():
-    print("Guess the letter: ")
-    return input()
-
-def process_letter(letter, secret_word, blanked_word):
-    result = False
-
-
-    for i in range(0, len(secret_word)):
-        if secret_word[i] == letter:
-            result = True
-            blanked_word[i] = letter
-    return result
+    return "pizza"
 
 def play_word_game():
     strikes = 0
     max_strikes = 3
-    playing = True
 
     word = get_random_word()
-    blanked_word = list( "_ " * len(word))
-
-    while playing:
-        show_word(blanked_word)
-        letter = get_guess()
-        found = process_letter(letter, word, blanked_word)
-
-
-        strikes += 1
-
-        if strikes >= max_strikes:
-            playing = False
-
-    if strikes >= max_strikes:
-        print("You lost")
-    else:
-        print("You are the Winner")
-
 
 
 print("Game started")
