c78157ee1af4a94fb13668cff6506fd875c82143
Pugilem <gytenis98@gmail.com>
2016-12-10 19:56:23 -0600

    Data structures and debugging lessons

---
+++ area.py
@@ -0,0 +1,15 @@
+'''
+    File name: area.py
+    Author: Gytenis Borusas
+    Date created: 12/06/2016
+    Date last modified: 12/06/2016
+    Python Version: 3.5
+
+    Debugging lesson file.
+'''
+
+base   = 10
+height = 5
+area   = 1/2 * (base*height)
+
+print("Are of our triangle is: ", area)
