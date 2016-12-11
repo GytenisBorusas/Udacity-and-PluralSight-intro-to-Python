c34a6f6e7dc9bae94cc82b69636095aae346b1b8
Pugilem <gytenis98@gmail.com>
2016-12-10 20:35:32 -0600

    Comments added.

--- shopping.py
+++ shopping.py
@@ -9,41 +9,44 @@
 '''
 
 
-def get_order():
+def get_order():         #First function called by go_shopping.
+    # Prints initial statement to the screen and divides the order
+    # into two variables - "command" and "item".
     print("[command] [item] (command is to add, d to delete, "
           "q to quit) ")
     line = input()
-
     command = line[:1]
     item = line[2:]
-
     return command, item
 
-def add_to_cart(item, cart):
+def add_to_cart(item, cart):        #defines how we deal with items
+    # which we already have in the list.
     if not item in cart:
         cart[item] = 0
     cart[item] += 1
 
-def delete_from_cart(item, cart):
+def delete_from_cart(item, cart):       #defines how we deal with items
+    # which we already have in the list.
     if item in cart:
         cart[item] -= 1
 
-def process_order(order, cart):
+def process_order(order, cart):     #It's the second function called
+    # from the go_shopping which defines how we deal with the inputs
+    # and call two more functions to deal with those inputs.
     command, item = order
-
     if command == "a":
         add_to_cart(item,cart)
     elif command == "d" and item in cart:
         delete_from_cart(item, cart)
     elif command == "q":
         return False
-
     return True
 
 
-def go_shopping():
+def go_shopping():      # Main function. Initializes "cart" dictionry
+    # list which is what we care about and what we will print to screen.
+    # And gives the "while" loop to check if the input is valid.
     cart = dict()
-
     while True:
         order = get_order()
         if not process_order(order, cart):
@@ -51,11 +54,5 @@ def go_shopping():
 
     print(cart)
     print("Finished!")
-
-
-
-
-
-
 
 go_shopping()
